4c44c80043cadb92acfb259c876c5fa818168539
chenaski <mail2chenaski@gmail.com>
2022-02-06 00:24:50 +0300

    Create `LingvoLive` parser

---
+++ src/LingvoLiveParser.ts
@@ -0,0 +1,53 @@
+import { GrepExamplesHook, Parser } from "./Parser.js";
+import { Examples } from "./Example.js";
+
+export class LingvoLiveParser {
+  static id = "lingvo-live";
+
+  async grepExamples({ dom }: Parameters<GrepExamplesHook>[0]): Promise<Examples> {
+    const examples = dom.window.document.querySelectorAll("[name='#quote'] blockquote");
+
+    if (!examples?.length) throw Error("examples not found");
+
+    const parsedExamples = [];
+
+    const getValue = (element: Element): string | null => {
+      return element.textContent;
+    };
+
+    for (const example of examples) {
+      const [enExampleNode, ruExampleNode] = example.children[0].children;
+
+      if (!enExampleNode || !ruExampleNode) continue;
+
+      const formatValue = (value: string): string => {
+        return value.replace(/\s+/g, " ").trim();
+      };
+
+      const enExample = getValue(enExampleNode);
+      const ruExample = getValue(ruExampleNode);
+
+      if (!enExample || !ruExample) continue;
+
+      parsedExamples.push({
+        ru: formatValue(enExample),
+        en: formatValue(ruExample),
+      });
+    }
+
+    return parsedExamples;
+  }
+
+  async run({ searchExpression }: { searchExpression: string }): Promise<Examples> {
+    const parser = new Parser({
+      hooks: {
+        grepExamples: this.grepExamples.bind(this),
+      },
+    });
+
+    const baseUrl = "https://www.lingvolive.com/en-us/translate/en-ru/";
+    const url = `${baseUrl}${encodeURIComponent(searchExpression)}`;
+
+    return parser.run({ url });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { YandexParser } from "./YandexParser.js";
 import { Cache } from "./Cache.js";
 import { WooordHuntParser } from "./WooordHuntParser.js";
+import { LingvoLiveParser } from "./LingvoLiveParser.js";
 
 async function getYandexExamples({ searchExpression }: { searchExpression: string }) {
   const yandexParser = new YandexParser();
@@ -32,12 +33,28 @@ async function getWooordHuntExamples({ searchExpression }: { searchExpression: s
   await cache.add(examples, searchExpression);
 }
 
+async function getLingvoLiveExamples({ searchExpression }: { searchExpression: string }) {
+  const lingvoLiveParser = new LingvoLiveParser();
+  const cache = new Cache({ id: LingvoLiveParser.id, cacheDir: "results" });
+
+  await cache.init();
+
+  if (await cache.has(searchExpression)) {
+    console.log("get from cache");
+    return cache.get(searchExpression);
+  }
+
+  const examples = await lingvoLiveParser.run({ searchExpression });
+  await cache.add(examples, searchExpression);
+}
+
 async function main() {
   try {
     const searchExpression = "bored stiff";
 
     // await getYandexExamples({searchExpression});
-    await getWooordHuntExamples({ searchExpression });
+    // await getWooordHuntExamples({ searchExpression });
+    await getLingvoLiveExamples({ searchExpression });
   } catch (e) {
     console.error(e);
   }
