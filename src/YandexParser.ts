91d1a9380424fe543bd7bb5f9c2f04778944394d
chenaski <mail2chenaski@gmail.com>
2022-02-07 22:39:43 +0300

    Simplify grab of examples for YandexParser

--- src/YandexParser.ts
+++ src/YandexParser.ts
@@ -14,28 +14,25 @@ export class YandexParser implements SiteParser {
   }
 
   async grepExamples({ dom }: Parameters<GrepExamplesHook>[0]): Promise<Examples> {
-    const examples = dom.window.document.querySelectorAll(".example_wrapper");
+    const examples = dom.window.document.querySelectorAll(".example_item");
+
+    if (!examples) throw Error("examples not found");
+
     const parsedExamples = [];
 
-    const getValue = (element: Element): string | null => {
-      return element?.children[0]?.textContent;
-    };
-
     for (const example of examples) {
-      const ruExample = getValue(example.children[0]);
-      const enExample = getValue(example.children[1]);
+      const enExample = example.getAttribute("data-text");
+      const ruExample = example.getAttribute("data-translation");
 
-      const formatValue = (value: string): string => {
-        return value.replace(/\s+/g, " ").trim();
-      };
-
-      if (!ruExample || !enExample) continue;
+      if (!enExample || !ruExample) continue;
 
       parsedExamples.push({
-        ru: formatValue(enExample),
-        en: formatValue(ruExample),
+        ru: ruExample,
+        en: enExample,
       });
     }
+
+    if (!parsedExamples.length) throw Error("examples not found");
 
     return parsedExamples;
   }
